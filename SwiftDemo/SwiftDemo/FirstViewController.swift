a8fef147d42f1e870fe78be7e5916f19bcf751bf
Sunny <zhang_zhi_li@163.com>
2016-10-18 07:40:34 +0800

    demo

---
+++ SwiftDemo/SwiftDemo/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  SwiftDemo
+//
+//  Created by Sunny on 16/10/17.
+//  Copyright © 2016年 Sunny. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
